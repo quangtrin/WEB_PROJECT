358687a2883b45f3e7a380e7e3e10708e68645fa
quangtrinhDSS <tienvu123d@gmail.com>
2023-01-01 11:50:59 +0700

    PW-37: create api post film, episodeFilm, category

---
+++ server/models/admin.js
@@ -0,0 +1,52 @@
+const adminModel = {
+  addEpisode: async (connect, filmID, episodeID, url, callback) => {
+    var sql = "INSERT INTO episode_film( filmID, episodeID, url ) VALUES ?";
+    const VALUES = [[Number(filmID), Number(episodeID), url]];
+    connect.query(sql, [VALUES], (err, result) => {
+      if (err) callback(err, null);
+      else callback(null, true);
+    });
+  },
+  addFilm: async (
+    connect,
+    filmName,
+    otherName,
+    status,
+    point,
+    year,
+    duration,
+    description,
+    categoryID,
+    url,
+    callback
+  ) => {
+    var sql =
+      "INSERT INTO film( filmName, otherName, status, point, year, duration, description, categoryID, url ) VALUES ?";
+    const VALUES = [
+      [
+        filmName,
+        otherName,
+        Number(status),
+        Number(point),
+        Number(year),
+        duration,
+        description,
+        Number(categoryID),
+        url,
+      ],
+    ];
+    connect.query(sql, [VALUES], (err, result) => {
+      if (err) callback(err, null);
+      else callback(null, true);
+    });
+  },
+  addCategory: async (connect, category, callback) => {
+    var sql =
+      "INSERT INTO category_film( category ) VALUES('" + category + "')";
+    connect.query(sql, (err, result) => {
+      if (err) callback(err, null);
+      else callback(null, true);
+    });
+  },
+};
+module.exports = adminModel;
