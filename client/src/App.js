40cf506a5a36e0333090f6eb71f94e4902c393b0
binhtrinh812 <binhtrinh812@gmail.com>
2023-01-06 16:27:35 +0700

    PW-21: Trang xem phim

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./page/Login";
 import Register from "./page/Register";
 import ListEpisode from "./page/ListEpisode/ListEpisode";
+import WatchFilm from "./page/WatchFilm/WatchFilm";
 
 function App() {
   return (
     <Routes>
       <Route path="/ListEpisode" element={<ListEpisode />} />
+      <Route path="/WatchFilm" element={<WatchFilm />} />
       <Route path="/Register" element={<Register />} />
       <Route path="/Login" element={<Login />} />
     </Routes>
