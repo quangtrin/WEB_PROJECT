e5ad867f6f1311573d682ae954fe4e752ce69244
quangtrin <Quang@gmail.com>
2023-01-07 11:12:28 +0700

    PW-45: add login/register for watchFilm

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home user={user} setIsSignUp={setIsSignUp} />} />
       <Route path="/ListEpisode" element={<ListEpisode user={user} setIsSignUp={setIsSignUp} />} />
-      <Route path="/WatchFilm" element={<WatchFilm />} />
+      <Route path="/WatchFilm" element={<WatchFilm user={user} setIsSignUp={setIsSignUp} />} />
       <Route path="/Register" element={<Register />} />
       <Route path="/Login" element={<Login setIsSignUp={setIsSignUp} />} />
     </Routes>
