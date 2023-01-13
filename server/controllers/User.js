50270a2f047b753dba4a6227dfc10ee692d734be
quangtrin <Quang@gmail.com>
2023-01-13 12:52:31 +0700

    PW-59: fix comment

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -56,9 +56,17 @@ const userController = {
   },
 
   getComment: async (req, res) => {
-    var sql = "SELECT commentID, comment, comment.userID, login_user.avatar, userName, commentParentID, DATE_FORMAT(time, '%H:%i %d-%m-%Y') AS time, likeCount, filmID, episodeID FROM comment JOIN login_user ON comment.userID = login_user.userID ORDER BY comment.time DESC";
+    const filmID = req.query?.filmID;
+    const episodeID = req.query?.episodeID !== "undefined" ? req.query.episodeID : null;
+    var sql;
+    if (episodeID === null) {
+      sql = "SELECT commentID, comment, comment.userID, login_user.avatar, userName, commentParentID, DATE_FORMAT(time, '%H:%i %d-%m-%Y') AS time, likeCount, filmID, episodeID FROM comment JOIN login_user ON comment.userID = login_user.userID AND comment.filmID = ? AND comment.episodeID IS ? ORDER BY comment.time DESC";
+    }
+    else {
+      sql = "SELECT commentID, comment, comment.userID, login_user.avatar, userName, commentParentID, DATE_FORMAT(time, '%H:%i %d-%m-%Y') AS time, likeCount, filmID, episodeID FROM comment JOIN login_user ON comment.userID = login_user.userID AND comment.filmID = ? AND comment.episodeID = ? ORDER BY comment.time DESC";
+    }
     try {
-      const [result] = await connect.query(sql);
+      const [result] = await connect.query(sql, [filmID, episodeID]);
       var comment = [];
       const commentParent = result.filter((comment) => {
         return comment.commentParentID === null;
