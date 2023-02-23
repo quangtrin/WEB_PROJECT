903b1c6f039653b3c5ad7e29a1dce1bbeb2e204b
binhtrinh812 <binhtrinh812@gmail.com>
2023-02-23 11:21:13 +0700

    PW-109

--- client/src/page/Admin/FilmAdmin/ListEpisodeFilm.js
+++ client/src/page/Admin/FilmAdmin/ListEpisodeFilm.js
@@ -6,8 +6,18 @@ import { useEffect, useState } from "react";
 import styles from "./FilmAdmin.module.scss";
 import FilmItem from "../../../components/Header/Search/FilmItem";
 
+
+import { FaSearch } from "react-icons/fa";
+import TableFilm from "./TableFilm/TableFilm";
+import { Link, useParams } from "react-router-dom";
+
 const cx = classNames.bind(styles);
 const ListEpisodeFilm = () => {
+  let { page } = useParams();
+  if (page === undefined || Number(page) <= 0) page = "1";
+
+  const [searchValue, setSearchValue] = useState("");
+
   const [filmName, setFilmName] = useState("");
   const [episodeID, setEpisodeID] = useState(0);
   const [episodeUrl, setEpisodeUrl] = useState("");
@@ -48,78 +58,46 @@ const ListEpisodeFilm = () => {
     }
     setIsHasData(true);
   };
-  //   e.preventDefault();
-  //   setSearchResult([]);
-  //   let isSuccess = true;
-  //   let id;
-  //   if (filmName && episodeID && episodeUrl) {
-  //     for (let i = 0; i < films.length; i++) {
-  //       if (filmName === films[i].filmName) {
-  //         id = films[i].filmID;
-  //       }
-  //     }
 
-  //     if (id) {
-  //       const list = await (
-  //         await axios.get("/api/episodeFilm/getEpisodeFilm/" + id)
-  //       ).data;
-  //       for (let index = 0; index < list.length; index++) {
-  //         if (list[index].episodeID == episodeID) {
-  //           isSuccess = false;
-  //           break;
-  //         }
-  //       }
+  const handleSearch = (e) => {
+    if (e.target.value[0] !== " ") setSearchValue(e.target.value);
+  };
 
-  //       if (isSuccess) {
-  //         const res = await axios.post("/api/episodeFilm/addEpisode", {
-  //           filmID: id,
-  //           episodeID: episodeID,
-  //           url: episodeUrl,
-  //         });
-  //         Swal.fire({
-  //           icon: "success",
-  //           title: "Success",
-  //           text: "Thêm thành công",
-  //           confirmButtonText: '<div class="fa fa-thumbs-up"}>OK</div>',
-  //         });
-  //       } else {
-  //         Swal.fire({
-  //           icon: "error",
-  //           title: "ERROR",
-  //           text: "Tập phim đã tồn tại",
-  //           confirmButtonText: '<div class="fa fa-thumbs-up"}>OK</div>',
-  //         });
-  //       }
-  //     } else {
-  //       Swal.fire({
-  //         icon: "error",
-  //         title: "ERROR",
-  //         text: "Tên phim không chính xác",
-  //         confirmButtonText: '<div class="fa fa-thumbs-up"}>OK</div>',
-  //       });
-  //     }
-  //   } else {
-  //     Swal.fire({
-  //       icon: "error",
-  //       title: "ERROR",
-  //       text: "Yêu cầu nhập đủ thông tin",
-  //       confirmButtonText: '<div class="fa fa-thumbs-up"}>OK</div>',
-  //     });
-  //   }
-  // };
+  const handleChangePage = () => {
+    window.scroll(0, 0);
+  };
+
 
   useEffect(() => {
     getDataFilms();
   }, []);
+
+  useEffect(() => {
+    if (films) {
+      if (searchValue !== "") {
+        const result = films.filter((film) => {
+          let checked = true;
+          for (let i = 0; i < searchValue.length; i++) {
+            if (
+              searchValue.toUpperCase()[i] !== film.filmName.toUpperCase()[i]
+            ) {
+              checked = false;
+            }
+          }
+          if (checked) return film;
+        });
+        setSearchResult(result);
+      } else setSearchResult(films);
+    }
+  }, [searchValue]);
 
   return (
     <div>
       {isHasData ? (
         <div className={cx("main-container")}>
           <div className={cx("card")}>
-            <section className={cx("container")}>
+            <section className={cx("attendance")}>
               <form action="#" className={cx("form")}>
-                <h1>Thêm tập</h1>
                 <HeadlessTippy
                   interactive
                   visible={searchResult.length > 0}
@@ -162,6 +140,165 @@ const ListEpisodeFilm = () => {
                   </div>
                 </HeadlessTippy>
               </form>
+              <div className={cx("attendance-list")}>
+              <h1>Danh sách tập phim</h1>
+              <div className={cx("header-table")}>
+                <div className={cx("search")}>
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    onChange={handleSearch}
+                    value={searchValue}
+                  ></input>
+                  <a href="#">
+                    <FaSearch />
+                  </a>
+                </div>
+              </div>
+              <table className={cx("table")}>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Tên phim</th>
+                    <th>Tập</th>
+                    <th>Url</th>
+                    <th>Thao tác</th>
+                  </tr>
+                </thead>
+                {isHasData ? (
+                  <tbody>
+                    {searchResult.map((film, index) => {
+                      if (index >= page * 30 - 30 && index <= 30 * page - 1)
+                        return (
+                          <TableFilm
+                            key={film.filmID}
+                            name={film.filmName}
+                          ></TableFilm>
+                        );
+                    })}
+                  </tbody>
+                ) : (
+                  <></>
+                )}
+              </table>
+              {isHasData ? (
+                <div className={cx("pagination")}>
+                  {Number(page) > 1 ? (
+                    <Link
+                      className={cx("prev")}
+                      to={"/Admin/ListFilm/" + (Number(page) - 1)}
+                      onClick={handleChangePage}
+                    >
+                      &lt;&lt;
+                    </Link>
+                  ) : (
+                    <></>
+                  )}
+                  <Link
+                    className={cx(
+                      "page",
+                      Number(page) <=
+                        Number.parseInt(searchResult.length / 30) + 1 - 3
+                        ? "active"
+                        : ""
+                    )}
+                    to={
+                      "/Admin/ListFilm/" +
+                      (Number(page) + 3 >
+                      Number.parseInt(searchResult.length / 30) + 1
+                        ? Number.parseInt(searchResult.length / 30) + 1 - 3
+                        : Number(page))
+                    }
+                    onClick={handleChangePage}
+                  >
+                    {Number(page) + 3 >
+                    Number.parseInt(searchResult.length / 30) + 1
+                      ? Number.parseInt(searchResult.length / 30) + 1 - 3
+                      : Number(page)}
+                  </Link>
+                  <Link
+                    className={cx(
+                      "page",
+                      Number(page) ===
+                        Number.parseInt(searchResult.length / 30) + 1 - 2
+                        ? "active"
+                        : ""
+                    )}
+                    to={
+                      "/Admin/ListFilm/" +
+                      (Number(page) + 3 >
+                      Number.parseInt(searchResult.length / 30) + 1
+                        ? Number.parseInt(searchResult.length / 30) + 1 - 2
+                        : Number(page) + 1)
+                    }
+                    onClick={handleChangePage}
+                  >
+                    {Number(page) + 3 >
+                    Number.parseInt(searchResult.length / 30) + 1
+                      ? Number.parseInt(searchResult.length / 30) + 1 - 2
+                      : Number(page) + 1}
+                  </Link>
+                  <Link
+                    className={cx(
+                      "page",
+                      Number(page) ===
+                        Number.parseInt(searchResult.length / 30) + 1 - 1
+                        ? "active"
+                        : ""
+                    )}
+                    to={
+                      "/Admin/ListFilm/" +
+                      (Number(page) + 3 >
+                      Number.parseInt(searchResult.length / 30) + 1
+                        ? Number.parseInt(searchResult.length / 30) + 1 - 1
+                        : Number(page) + 2)
+                    }
+                    onClick={handleChangePage}
+                  >
+                    {Number(page) + 3 >
+                    Number.parseInt(searchResult.length / 30) + 1
+                      ? Number.parseInt(searchResult.length / 30) + 1 - 1
+                      : Number(page) + 2}
+                  </Link>
+                  <Link
+                    className={cx(
+                      "page",
+                      Number(page) ===
+                        Number.parseInt(searchResult.length / 30) + 1
+                        ? "active"
+                        : ""
+                    )}
+                    to={
+                      "/Admin/ListFilm/" +
+                      (Number(page) + 3 >
+                      Number.parseInt(searchResult.length / 30) + 1
+                        ? Number.parseInt(searchResult.length / 30) + 1
+                        : Number(page) + 3)
+                    }
+                    onClick={handleChangePage}
+                  >
+                    {Number(page) + 3 >
+                    Number.parseInt(searchResult.length / 30) + 1
+                      ? Number.parseInt(searchResult.length / 30) + 1
+                      : Number(page) + 3}
+                  </Link>
+                  {Number(page) + 3 <
+                  Number.parseInt(searchResult.length / 30) + 1 ? (
+                    <Link
+                      className={cx("next")}
+                      to={"/Admin/ListFilm/" + (Number(page) + 1)}
+                      onClick={handleChangePage}
+                    >
+                      &gt;&gt;
+                    </Link>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
             </section>
           </div>
         </div>
