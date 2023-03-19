7c1db1932382e2b756978401c8d2c765f2a1159e
= <=>
2023-03-19 14:36:46 +0700

    PW-132: sua UI header

--- client/src/components/Admin/HeaderAdmin/HeaderAdmin.js
+++ client/src/components/Admin/HeaderAdmin/HeaderAdmin.js
@@ -1,10 +1,10 @@
 import imgLogo from "../../../imgs/logo_hqbh.png";
-
 import classNames from "classnames/bind";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 import styles from "./HeaderAdmin.module.scss";
 
 import { useNavigate } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
 import { FaBell } from "react-icons/fa";
 import { FaPowerOff } from "react-icons/fa";
 
@@ -12,19 +12,34 @@ const cx = classNames.bind(styles);
 
 const HeaderAdmin = () => {
   const navigate = useNavigate();
+
   const handleLogout = () => {
     localStorage.clear();
-    navigate("/AdminLogin")
-  }
+    navigate("/AdminLogin");
+  };
+
   return (
     <>
       <header className={cx("header")}>
         <div className={cx("header-menu")}>
           <img className={cx("header-logo")} src={imgLogo}></img>
           <ul>
-            <li><a href="#"><p><FaSearch /></p></a></li>
-            <li><a href="#"><p><FaBell /></p></a></li>
-            <li><a href="#" onClick={handleLogout}><p><FaPowerOff /></p></a></li>
+            <li>
+              <a href="#">
+                <p>
+                  <FaBell />
+                </p>
+              </a>
+            </li>
+            <Tippy content="Đăng Xuất" theme="light-border">
+              <li>
+                <a href="#" onClick={handleLogout}>
+                  <p>
+                    <FaPowerOff />
+                  </p>
+                </a>
+              </li>
+            </Tippy>
           </ul>
         </div>
       </header>
