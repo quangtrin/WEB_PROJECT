b9f23686cfd627f8f4a4a587a7752193291a4573
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2023-04-01 09:31:26 +0700

    PW-140:[NODE] Tạo api sửa phim

--- server/controllers/film.js
+++ server/controllers/film.js
@@ -1,44 +1,78 @@
 import { connect } from "../db.js";
 
 const filmController = {
-    addFilm: async (req, res) => {
-        const {
-            filmName,
-            status,
-            year,
-            duration,
-            description,
-            category,
-            url,
-        } = req.body;
+  addFilm: async (req, res) => {
+    const { filmName, status, year, duration, description, category, url } =
+      req.body;
 
-        var sql = "INSERT INTO film( filmName, status, year, duration, description, category, url ) VALUES (?,?,?,?,?,?,?)";
-        try {
-            const [result] = await connect.query(sql, [filmName, status, year, duration, description, category, url]);
-            res.json(true);
-        } catch (error) {
-            console.log(error);
-            res.json(false);
-        }
-    },
-    getFilm: async (req, res) => {
-        var sql = "SELECT film.*, COUNT(DISTINCT episode_film.episodeID) as episodeCount, AVG(rate_film.rate) as point FROM film LEFT JOIN  episode_film ON film.filmID = episode_film.filmID LEFT JOIN rate_film ON film.filmID = rate_film.filmID  GROUP BY filmID order by COUNT(episode_film.filmID) DESC";
-        try {
-            const [result] = await connect.query(sql);
-            res.json(result)
-        } catch (error) {
-            console.log(error);
-        }
+    var sql =
+      "INSERT INTO film( filmName, status, year, duration, description, category, url ) VALUES (?,?,?,?,?,?,?)";
+    try {
+      const [result] = await connect.query(sql, [
+        filmName,
+        status,
+        year,
+        duration,
+        description,
+        category,
+        url,
+      ]);
+      res.json(true);
+    } catch (error) {
+      console.log(error);
+      res.json(false);
+    }
+  },
+  getFilm: async (req, res) => {
+    var sql =
+      "SELECT film.*, COUNT(DISTINCT episode_film.episodeID) as episodeCount, AVG(rate_film.rate) as point FROM film LEFT JOIN  episode_film ON film.filmID = episode_film.filmID LEFT JOIN rate_film ON film.filmID = rate_film.filmID  GROUP BY filmID order by COUNT(episode_film.filmID) DESC";
+    try {
+      const [result] = await connect.query(sql);
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getFilmByName: async (req, res) => {
+    const filmName = req.params.name;
+    var sql =
+      "SELECT film.*, ROUND(AVG(rate), 2) as point FROM film LEFT JOIN rate_film ON rate_film.filmID = film.filmID WHERE filmName = " +
+      "'" +
+      filmName +
+      "'" +
+      "GROUP BY filmID";
+    try {
+      const [result] = await connect.query(sql);
+      res.json(result[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    },
-    getFilmByName: async (req, res) => {
-        const filmName = req.params.name;
-        var sql = "SELECT film.*, ROUND(AVG(rate), 2) as point FROM film LEFT JOIN rate_film ON rate_film.filmID = film.filmID WHERE filmName = " + "'" + filmName + "'" + "GROUP BY filmID";
-        try {
-            const [result] = await connect.query(sql);
-            res.json(result[0]);
-        } catch (error) { console.log(error) };
-    },
-}
+  updateFilm: async (req, res) => {
+    const filmID = req.params.id;
+    const { filmName, status, year, duration, description, category, url } =
+      req.body;
+
+    const sql =
+      "UPDATE film SET filmName = ?, status = ?,year = ?,duration = ?,description = ?,category = ?,url = ? WHERE filmID=?";
+    try {
+      const [result] = await connect.query(sql, [
+        filmName,
+        status,
+        year,
+        duration,
+        description,
+        category,
+        url,
+        filmID,
+      ]);
+      res.json("Update thành công");
+    } catch (error) {
+      console.log(error);
+      res.json("Update thất bại");
+    }
+  },
+};
 
 export default filmController;
